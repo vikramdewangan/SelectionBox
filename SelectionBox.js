a4434807b4fcb1b7c4c4853c9fdfdb72b4b58424
Vikram Dewangan <vikramdewangan@outlook.com>
2022-04-25 07:07:49 +0530

    Create SelectionBox.js

---
+++ SelectionBox.js
@@ -0,0 +1,50 @@
+import "./SelectionBox.css";
+
+function SelectionBox() {
+  return (
+    <div className="root">
+      <div className="row">
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+      </div>
+      <div className="row">
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+      </div>
+      <div className="row">
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+      </div>
+      <div className="row">
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+      </div>
+      <div className="row">
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+        <div className="box" />
+      </div>
+    </div>
+  );
+}
+
+export default SelectionBox;
